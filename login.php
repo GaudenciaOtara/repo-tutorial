76be97a2f412a83b541fbeb63d85fa4d0006d56e
Gaudencia <78945952+Gaudencia-Otara@users.noreply.github.com>
2023-04-07 10:25:38 +0300

    new repo push commit

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription Sign Up</title>
+    <link rel="stylesheet" href="css/signup.css">
+</head>
+<body>
+    <h2>Login to Your Account</h2>
+    <div class="form">
+        <form action="">
+            <input type="email" placeholder="Email" required><br>
+            <input type="password" placeholder="Password" required><br>
+        
+            <button class="signup">Login</button>
+            <p class="link" style="margin-left:25%;";>Don't have an account? <a href="signup.php">SignUp</a></p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription Sign Up</title>
+    <link rel="stylesheet" href="css/signup.css">
+</head>
+<body>
+    <h2>Register Your Account</h2>
+    <div class="form">
+        <form action="">
+            <input type="text" placeholder="First Name" required>
+            <input type="text" placeholder="Second Name" required><br>
+            <input type="email" placeholder="Email" required>
+            <input type="password" placeholder="Password" required><br>
+            <input type="tel" placeholder="Phone Number" required>
+            <input type="text" placeholder="Address" required><br>
+            <button class="signup">SIGN UP</button>
+            <p class="link">Already have an account? <a href="login.php">Login</a></p>
+        </form>
+    </div>
+</body>
+</html>
